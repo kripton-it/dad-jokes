b6a93ce7f5641a1cdfcbed97b01e22767c50ab55
Mikhail Shardin <kriptonit1988@gmail.com>
2019-06-29 01:38:58 +0500

    Updates

---
+++ precache-manifest.44b57babb629b14776da4a3e93e684ed.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbb0dae1e5982fcad97a1216e7322d07",
+    "url": "/dad-jokes/index.html"
+  },
+  {
+    "revision": "fe769d3706f1a75cf798",
+    "url": "/dad-jokes/static/css/main.f2fa87a4.chunk.css"
+  },
+  {
+    "revision": "8f1e2ba09f13b21b4466",
+    "url": "/dad-jokes/static/js/2.19f62289.chunk.js"
+  },
+  {
+    "revision": "fe769d3706f1a75cf798",
+    "url": "/dad-jokes/static/js/main.19a0238a.chunk.js"
+  },
+  {
+    "revision": "03aa9e7745c6e7ee15e7",
+    "url": "/dad-jokes/static/js/runtime~main.c81b4217.js"
+  }
+]);
